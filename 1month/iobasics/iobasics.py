b528c6cb9d7130d3451ccef862df91ab44875e9c
Raghav14200 <raghav142001@gmail.com>
2021-04-29 16:22:39 +0530

    iobasics

--- 1month/iobasics/iobasics.py
+++ 1month/iobasics/iobasics.py
@@ -6,4 +6,43 @@
 "t" -> text mode.
 "b" -> binary mode.
 "+" -> read and write.
-""""
+"""
+
+#read file named sample.txt
+file=open("sample.txt")
+print(file.readlines())
+file.close()
+
+file=open("sample.txt")
+content=file.read()
+print(content)
+file.close()
+
+#write into file.
+file=open("sample2.txt","w")
+file.write("I am writing into file using python console")
+file.close()
+
+#Append into file.
+file=open("sample.txt","a")
+length=file.write("I have appended in sample file")
+print(length)#returns the length that has been appended.
+file.close()
+
+file=open("sample2.txt","r")
+print(file.read())
+file.close()
+
+#handle read and write both
+file=open("sample2.txt","r+")
+print(file.read())
+file.write("Thanks for your corporation")
+print(file.read())
+file.close()
+
+
+
+
+
+
+
